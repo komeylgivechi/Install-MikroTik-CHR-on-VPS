1ab1616fa2bb466c4169f7ad1f67828c76cb7999
ikomeyl <59527220+komeylgivechi@users.noreply.github.com>
2025-02-24 01:16:42 +0330

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -2,7 +2,7 @@
 
 echo
 echo
-sleep 3
+sleep 1
 wget https://download.mikrotik.com/routeros/7.11.2/chr-7.11.2.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
@@ -13,7 +13,7 @@ ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
 echo ADDRESS is $ADDRESS && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo GATEWAY is $GATEWAY && \
-sleep 5 && \
+sleep 3 && \
 dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
 echo "Ok, reboot" && \
 echo 1 > /proc/sys/kernel/sysrq && \
